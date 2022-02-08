f09bf53464610582cb948795da2ca6ee034ebcb1
999SH <94057132+999SH@users.noreply.github.com>
2022-02-08 08:36:29 -0500

    Add files via upload

--- Mipsfiles/timetemplate.asm
+++ Mipsfiles/timetemplate.asm
@@ -72,20 +72,94 @@ tiend:	sw	$t0,0($a0)	# save updated result
 	jr	$ra		# return
 	nop
 
-
-  # you can write your code for subroutine "hexasc" below this line
-  #
 hexasc:
 
-	andi $a0, 0xf	
-	addi $a0, $a0, 0x30
-	bge  $a0,0x39, greater
-	nop
-	
-	jr $ra
-	nop
+    andi $v0, $a0, 0xf    	#Copies the 4 lsb of $a0
+    addi $v0, $v0, 0x30     	#Adds hex 30 to number since that is where 1 starts
+    bge  $v0, 0x3a, greater 	#If the number is greater or equal to hex 39 (a) then it will jump to the greater function.
+    nop                     	#This will not be needed in this program since the time cannot be alphabetical letters
+
+    jr $ra
+    nop
+
 greater: 
-	addi $a0 ,$a0, 7
-	jr $ra
-	nop
+    addi $v0 ,$v0, 7
+    jr $ra
+    nop
+
+delay: 
+    		PUSH ($ra) 			#$ra must have the same contents as when the subroutine was called 
+    		li $a0, -1
+    		move $t0, $a0        		#store $a0 from time2string in a temporary register 
+	while: 
+    		blez $t0, leave_loop     	#if $t0 greater than zero branch to leave_loop 
+    		nop
+    		addi $t0, $t0, -1    		#ms = ms - 1 
+
+    		li $t1, 0        		#int i = 0 
+	for:
+    		bge $t1, 4711, while   		#if $t1 is greater than or equal to 1000 branch to while
+    		nop 
+    		addi $t1, $t1, 1    		#i = i + 1
+    		j for             		#runs loop until condition is fulfilled 
+    		nop 
 	
+	leave_loop: 
+    		POP ($ra)        		#$ra value retreived from stack 
+    		jr $ra
+    		nop
+time2string:
+	PUSH	($s0)				# Save contents of s1 to restore it after the function ends
+	PUSH	($ra)				# Save the return adress on the stack
+	move	$s1, $a1			# Move contents of $a1 to $s1 so we can work with it
+	move	$s0, $a0
+
+	# First digit
+	andi 	$t1, $a1, 0xf000		# Masking out bit from index 15 to 12
+	srl 	$a0, $t1, 12			# Shifting the bits to lowest position and store it in $a0 for hexas
+	jal	hexasc				# Calling the hexasc that will transform the decimal into hexadecimal
+	nop
+	sb 	$v0, 0($s0)		 	# Save the return value from hexasc in the first byte location $s1 
+						# points to					
+
+	# Second digit
+	andi 	$t1, $a1, 0x0f00		# Masking out bit from index 11 to 8
+	srl 	$a0, $t1, 8			# Shifting the bits to lowest position and store it in $a0 for hexasc
+	jal	hexasc				# Calling the hexasc that will transform the decimal into hexadecimal
+	nop
+	sb 	$v0, 1($s0)		 	# Save the return value from hexasc in the second byte location $s1 
+						# points to					
+
+	# Adding the colon
+	li 	$t1, 0x3a			# Loading the ASCII code for colon
+	sb 	$t1, 2($s0)		 	# Save the return value from hexasc in the third byte location $s1 
+						# points to
+	
+	# Third digit
+	andi 	$t1, $a1, 0x00f0		# Masking out bit from index 7 to 4
+	srl 	$a0, $t1, 4			# Shifting the bits to lowest position and store it in $a0 for hexasc
+	jal	hexasc				# Calling the hexasc that will transform the decimal into hexadecimal
+	nop
+	sb 	$v0, 3($s0)		 	# Save the return value from hexasc in the fourth byte location $s1 
+						# points to
+										
+	# Forth digit
+	andi 	$t1, $a1, 0x000f		# Masking out bit from index 3 to 0
+	move 	$a0, $t1			# No need for shifting, just move it to the argument.
+	jal	hexasc				# Calling the hexasc that will transform the decimal into hexadecimal
+	nop
+	sb 	$v0, 4($s0)		 	# Save the return value from hexasc in the fifth byte location $s1 
+						# points to
+
+	# Adding the NUL byte
+	li	$t1, 0x00			# Loading the ASCII code for NUL
+	sb 	$t1, 5($s0)		 	# Save the return value from hexasc in the sixth byte location $s1 
+	j	exit_timesubroutine		# points to
+
+	# End of subroutine. Restoring registers and jumping back to caller.
+exit_timesubroutine:																																																																																										
+	POP	($ra)
+	POP	($s0)	
+ 	jr 	$ra
+ 	nop	
+
